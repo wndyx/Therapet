3489bf78412a54ac34eece94bf197e3f4de7d872
Pohkee <53234621+Pohkee@users.noreply.github.com>
2024-11-10 13:07:30 -0500

    Updated db.js
    - Use new connection string for proper database
    - Made some other changes BUT NEED TO CHECK THEM

--- db.js
+++ db.js
@@ -1,14 +1,15 @@
-// db.js
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
+// Replace "<database-name>" with the actual database you want to connect to
+mongoose.connect("mongodb+srv://kareenamehta:Pohkee123@firstcluster.wmuhn.mongodb.net/<UsersDatabase>?retryWrites=true&w=majority")
+    .then(() => {
+        console.log("Connected to MongoDB!");
+    })
+    .catch(err => {
+        console.error("MongoDB connection error:", err);
+    });
 
-
-
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+module.exports = mongoose;
